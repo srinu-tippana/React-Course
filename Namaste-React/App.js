902a4904866b505f6bfafcd409755a60e0fc4089
srinu-tippana <svas7246@gmail.com>
2023-06-24 19:51:17 +0530

    React First episode

---
+++ Namaste-React/App.js
@@ -0,0 +1,57 @@
+/**
+ * <div id ="child">
+ *     <div id="parent">
+ *      </div>
+ * </div>
+ */
+
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "Hello world from React"
+);
+
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I am an h1 tag"),
+    React.createElement("h2", {}, "I am an h2 tag"),
+  ])
+);
+
+/*<div>
+    <div>
+          <h1>
+          </h1>
+          <h2>
+            
+          </h2>
+    </div>
+    <div>
+          <h1>
+          </h1>
+          <h2>
+            
+          </h2>
+    </div>
+</div>*/
+
+const secondParent = React.createElement(
+  "div",
+  { id: "top parent" },[
+    React.createElement("div", { id: "child1" }, [
+      React.createElement("h1", {}, "I am an h1 tag"),
+      React.createElement("h2", {}, "I am an h2 tag"),
+    ]),
+    React.createElement("div", { id: "child2" }, [
+      React.createElement("h1", {}, "I am an h1 tag"),
+      React.createElement("h2", {}, "I am an h2 tag"),
+    ])
+
+  ]
+
+);
+console.log(heading);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(secondParent);
