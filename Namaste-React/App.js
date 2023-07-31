6f8e9ae15e0b8e0a5b4222ad3288c2b857323608
srinu_tippana <svas7246@gmail.com>
2023-07-31 23:25:36 +0530

    Food App

--- Namaste-React/App.js
+++ Namaste-React/App.js
@@ -6,79 +6,66 @@
  */
 import React from "react";
 import  ReactDOM from "react-dom";
-const heading = React.createElement(
-  "h1",
-  { id: "heading" },
-  "Hello world from React"
-);
 
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I am an h1 tag"),
-    React.createElement("h2", {}, "I am an h2 tag"),
-  ])
-);
-
-/*<div>
-    <div>
-          <h1>
-          </h1>
-          <h2>
-            
-          </h2>
-    </div>
-    <div>
-          <h1>
-          </h1>
-          <h2>
-            
-          </h2>
-    </div>
-</div>*/
-
-/*const secondParent = React.createElement(
-  "div",
-  { id: "top parent" },[
-    React.createElement("div", { id: "child1" }, [
-      React.createElement("h1", {}, "I am an h1 tag"),
-      React.createElement("h2", {}, "I am an h2 tag"),
-    ]),
-    React.createElement("div", { id: "child2" }, [
-      React.createElement("h1", {}, "I am an h1 tag"),
-      React.createElement("h2", {}, "I am an h2 tag"),
-    ])
-
-  ]
-
-);*/
 /*
-   Jsx is HTML/XML like syntax
-*/
-// JSX->Babel transpiles it to React.createElement => React Object  => renders to HTML
- const jsxHeading=<h1 id ="heading" tabIndex="5"> Namaste React using Jsx</h1>
 
- /*const HeadingComponent =()=>(
+Header
+ Logo 
+ Navbar
 
-  <h1 className="heading"> Namaste React using functional component </h1>
- );*/
-
- const Title = () =>  ( 
- <h1 className="title" >
-      Namaste React Using Jsx
-  </h1>
-
- )
+Body
  
- /* Component written in inside Component Composition */
- const HeadingComponent = ()=>(
-  
-  <div id="container">
-    <Title/>
-    <h1> Hello from React FunctionalComponent</h1>
-  </div>
- )
-console.log(heading);
+ Search 
+ Restaruant container  and within restaruant cards
+
+
+
+Footer
+
+ Copy righst and other links 
+*/
+
+const Header =()=>{
+  return(
+    <div className="header">
+      <div className="logo-container">
+            <img  className="logo" src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png"></img>
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+
+      </div>
+
+    </div>
+  );
+}
+
+const Body =() =>{
+  return(
+    <div className="body">
+      <div className="search">
+
+      </div>
+
+    </div>
+
+  );
+};
+
+const AppLayout = ()=>{
+
+  return(
+    <div className="app">
+       <Header/>
+    </div>
+  )
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<HeadingComponent/>);
+
+root.render(<AppLayout/>)
